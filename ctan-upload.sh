762829e7d6c07a003aaa8b54a0552d39fc8ff989
Vít Novotný <witiko@gmail.com>
2016-03-31 14:53:17 +0200

    Updated the error messages.

--- ctan-upload.sh
+++ ctan-upload.sh
@@ -5,23 +5,24 @@ die() { printf '%s\n' "${*:2}"; exit $1; }
 [ -e "$1" ] && . "$1"
 
 # Perform sanity checks.
-[ -z "$PKG" ] && die 1 Undefined PKG
-[ -z "$VERS" ] && die 2 Undefined VERS
-[ -z "$AUTHOR" ] && die 3 Undefined AUTHOR
-[ ! -e "$FILENAME" ] && die 4 Undefined FILENAME
-[ -z "$EMAIL" ] && die 5 Undefined EMAIL
-[ -z "$DESCRIPTION" ] && die 6 Undefined DESCRIPTION
-[ -z "$CTANPATH" ] && die 7 Undefined CTANPATH
-[ -z "$TYPE" ] && die 8 Undefined TYPE
+[ -z "$PKG" ] && die 1 Undefined / empty PKG
+[ -z "$VERS" ] && die 2 Undefined / empty VERS
+[ -z "$AUTHOR" ] && die 3 Undefined / empty AUTHOR
+[ -z "$FILENAME" ] && die 4 Undefined / empty FILENAME
+[ ! -e "$FILENAME" ] && die 5 File FILENAME does not exist
+[ -z "$EMAIL" ] && die 6 Undefined / empty EMAIL
+[ -z "$DESCRIPTION" ] && die 7 Undefined / empty DESCRIPTION
+[ -z "$CTANPATH" ] && die 8 Undefined / empty CTANPATH
+[ -z "$TYPE" ] && die 9 Undefined / empty TYPE
 [ "$TYPE" = announce -a -z "$ANNOUNCEMENT" ] &&
-  die 9 TYPE is announce, but ANNOUNCEMENT is undefined
+  die 10 TYPE is announce, but ANNOUNCEMENT is undefined / empty
 
 # Retrieve a ticket number from CTAN.
 COOKIEJAR=`mktemp`
 trap 'rm $COOKIEJAR' EXIT
 TICKET="$(curl -c $COOKIEJAR -s 'https://ctan.org/upload' |
   sed -nr '/<input name="ticket"/s/.*<input name="ticket".*value="([^"]*)".*/\1/p')"
-[ -z "$TICKET" ] && die 10 Failed to download ticket number.
+[ -z "$TICKET" ] && die 11 Failed to download ticket number.
 
 # Send the archive.
 RESPONSE=`mktemp`
@@ -42,4 +43,4 @@ curl -F ticket="$TICKET" \
      -F SUBMIT='Submit contribution' \
      -b $COOKIEJAR https://ctan.org/upload/save | tee $RESPONSE
   grep <$RESPONSE -qF 'Your contribution has been uploaded' ||
-  die 11 Upload failed: "`cat $RESPONSE`"
+  die 12 Upload failed: "`cat $RESPONSE`"
